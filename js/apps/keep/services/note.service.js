b0e304ce61fd9d16057d2858d6bdc710ee1f5472
opal pinchas <opalp10145@gmail.com>
2022-04-27 15:03:16 +0300

    add note service

---
+++ js/apps/keep/services/note.service.js
@@ -0,0 +1,52 @@
+import { storageService } from "../../../services/storage.service.js"
+
+export const noteService = {
+    query
+}
+
+const KEY = 'notesDB'
+
+_creatNotes()
+
+
+
+function query() {
+    let notes = _loadFromStorage()
+    return Promise.resolve(notes)
+}
+
+
+
+
+function _creatNotes() {
+    const notes = _loadFromStorage()
+    if (!notes) {
+        const notes = [
+            {
+                id: "n10111",
+                type: "note-txt",
+                isPinned: true,
+                info: {
+                    txt: "Fullstack Me Baby!"
+                }
+            },
+            {
+                id: "n101",
+                type: "note-txt",
+                isPinned: true,
+                info: {
+                    txt: "Fullstack Me Baby!2"
+                }
+            }]
+        _saveToStorage(notes)
+    }
+}
+
+
+function _saveToStorage(notes) {
+    storageService.saveToStorage(KEY, notes)
+}
+function _loadFromStorage() {
+    return storageService.loadFromStorage(KEY)
+}
+
